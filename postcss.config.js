67d6ce503cb6eb661901680844175fd1021ec79d
BSK-Media <kontakt@bskmedia.pl>
2026-02-01 10:45:15 +0100

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,10 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+// postcss.config.js
+// NOTE: This repo uses `"type": "module"` in package.json, so this config must be ESM.
+// If you prefer CommonJS, rename this file to `postcss.config.cjs` and use `module.exports = ...`.
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
